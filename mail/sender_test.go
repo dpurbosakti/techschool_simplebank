8381e5223183a6288f8247b6fe180b8d8cd304f1
dpurbosakti <dwiatmokop@gmail.com>
2024-01-31 17:19:28 +0700

    add logger for asynq error/redis error

---
+++ mail/sender_test.go
@@ -0,0 +1,13 @@
+package mail
+
+// import (
+// 	"simple-bank/util"
+// 	"testing"
+// )
+
+// func TestSendEmailWithGmail(t *testing.T) {
+// 	config, err := util.LoadConfig("..")
+// 	require.NoError(t, err)
+
+// 	sender := NewGmailSender(config.EmailSenderName, config.EmailSenderAddress, config.EmailSenderPassword)
+// }
